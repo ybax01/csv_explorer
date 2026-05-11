396369c937ea26372fc0514eb50cb9ee1a9c7b35
ybax01 <youssef1002016@gmail.com>
2026-05-11 22:30:43 +0100

    titre et size par défaut

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 app = QApplication(sys.argv)
 
 window = QMainWindow()
+window.setWindowTitle("Explorateur CSV")
+window.resize(1280, 720)
 window.show()
 
 sys.exit(app.exec_())
